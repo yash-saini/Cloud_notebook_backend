44e0762009be5ccb9247d2a9d1705bcc433740c0
Yash Saini <yash.0245113@gmail.com>
2023-07-16 00:09:02 +0530

    Backend updated

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,21 @@
 const express = require('express')
-const router = express.Router();
 
+const router = express.Router();
+const User = require('../models/Users')
 
 router.get('/', (req, res) => {
-  obj ={ id : 12,
-number : 345}
-res.json(obj);
+    // obj={
+    
+    // username: "Yash",
+    // email: "xyz@gmail.com",
+    // password: "asdasdasdd"
+    //     }
+    
+    console.log(req.body);
+    const user= User(req.body);
+    user.save();
+    res.send(req.body);
+
 })
 
 module.exports= router;
