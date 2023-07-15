f85f15c339eef2f5398b09ae492874df087daff2
Yash Saini <yash.0245113@gmail.com>
2023-07-15 16:26:32 +0530

    backend

---
+++ db.js
@@ -0,0 +1,13 @@
+
+const mongoose=require('mongoose')
+const mongURL= "mongodb://localhost:27017/"
+mongoose.connect(mongURL)
+
+const connect_To_Mongoose=async ()=>
+{
+    //const check=await fetch(mongURL)
+    console.log("Inside function")
+    //const data=await check.json()
+
+}
+module.exports=connect_To_Mongoose
---
+++ index.js
@@ -0,0 +1,14 @@
+const connector=require("./db")
+connector()
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
