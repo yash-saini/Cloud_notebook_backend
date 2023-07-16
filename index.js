ea403d939c92869108bc4aa32fcf24a61028298b
Yash Saini <yash.0245113@gmail.com>
2023-07-16 23:41:18 +0530

    login func

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ connector()
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 
 app.use(express.json()) //middleware
 //Routes which are present
--- routes/login.js
+++ routes/login.js
@@ -4,27 +4,52 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const User = require('../models/Users')
 
-router.post('/', [    body('username','Enter a valid username').isLength({min : 5}),
+router.post('/', [body('username','Enter a valid username').isLength({min : 5}),
 body('email','Enter a valid email').isEmail(),
 body('password','Enter a valid password').isLength({min : 8 })
-
 ]
-,(req, res) => {
+
+,async (req, res) => {
     
     const errors= validationResult(req);
     if(!errors.isEmpty())
     {
         return res.status(400).json({errors:errors.array()});
     }
-    User.create({username: req.body.username,
-                 password: req.body.password,
-                 email: req.body.email}).then(user => res.json(user)).catch(err=>{console.log(err)
-                res.json({error:'Please enter a unique values for email and username',message: err.message})
-                
-                
-                });
+    
+ 
+    try {    //check for unique emails
+        let user= await User.findOne({email: req.body.email});
+        if (user)
+        {
+            return res.status(400).json({error: "Sorry a user with the same email already exists"})
+        }
+    
+        //check for unique username
+        user= await User.findOne({username: req.body.username})
+    
+        if (user)
+        {
+            return res.status(400).json({error: "Sorry a user with the same username already exists"})
+        }
+    
+    
+    
+        
+        user= await User.create({username: req.body.username,
+                     password: req.body.password,
+                     email: req.body.email})
+                    //  .then(user => res.json(user)).catch(err=>{console.log(err)
+                    // res.json({error:'Please enter a unique values for email and username',message: err.message})});
+    
+                res.json(user)
+      }
+   catch(error) {
+    
+    console.error(error.message);
+    res.status(500).send("Some error occurred");
 
-
+                }
 })
 
 module.exports= router;
